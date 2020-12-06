9f05dac0ca5025fd001311c62843ccf2784a8516
Colin Roberts <colin_roberts1@hotmail.co.uk>
2020-12-06 17:39:48 +0000

    Testing

--- KataShoppingCart.Tests/Controllers/CartController.Test.cs
+++ KataShoppingCart.Tests/Controllers/CartController.Test.cs
@@ -1,14 +1,27 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+using KataShoppingCart;
+using KataShoppingCart.Controllers;
 
 namespace KataShoppingCart.Tests.Controllers
 {
     [TestClass]
-    public class UnitTest1
+    public class CartControllerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Index()
         {
+            // Arrange
+            CartController controller = new CartController();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.IsNotNull(result);
         }
     }
 }
--- KataShoppingCart.Tests/Controllers/ProductController.Test.cs
+++ KataShoppingCart.Tests/Controllers/ProductController.Test.cs
@@ -1,14 +1,27 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+using KataShoppingCart;
+using KataShoppingCart.Controllers;
 
 namespace KataShoppingCart.Tests.Controllers
 {
     [TestClass]
-    public class UnitTest2
+    public class ProductControllerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Index()
         {
+            // Arrange
+            ProductController controller = new ProductController();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.IsNotNull(result);
         }
     }
 }
